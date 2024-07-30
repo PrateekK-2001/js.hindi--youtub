8a657af8248e428c05d752dfee86372bc5e77427
Prateek Kumar Patel <153807112+PrateekK-2001@users.noreply.github.com>
2024-07-30 04:07:07 +0000

    datatype

---
+++ 01_basics/o1_variable.js
@@ -0,0 +1,10 @@
+const accountId = 12324224
+let  accountEmail = "prateekkp20@gamil.con"
+var accountPasswoed = "12131"// not use var only use let 
+accountCity = "varansi"
+// prefer not to  use var because of 
+//issue in the block scope and function scope
+
+let accountState;
+console.log(accountId);
+console.table([accountId,accountEmail,accountPasswoed,accountCity,accountState]);
